fbee714c087526c0d68c982b040b1e99935ead99
Daniel Haddington <operas.referee.0e@icloud.com>
2023-08-16 11:28:18 +0930

    setting up for input

--- Makefile
+++ Makefile
@@ -3,12 +3,13 @@ NAME 	:= yosh.i
 LIBFT_A := libft/libft.a
 LIBFT 	:= libft/
 
+LINKL	:= -lreadline
 X86   	:= arch -x86_64
 FLAGS	:= -Wextra -Werror -Wall
 ADDSAN  := -g -fsanitize=address
 SOURCE 	:= source/*.c
 
-COMPILE = gcc $(FLAGS) $(SOURCE) $(LIBFT_A) -o $(NAME)
+COMPILE = gcc $(FLAGS) $(SOURCE) $(LINKL) $(LIBFT_A) -o $(NAME)
 
 all:
 			$(X86) make -C $(LIBFT)
---
+++ includes/yosh_i.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   yosh_i.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhadding <operas.referee.0e@icloud.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/16 10:31:51 by dhadding          #+#    #+#             */
+/*   Updated: 2023/08/16 10:51:38 by dhadding         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef YOSH_I_H
+# define YOSH_I_H
+
+# define INLEN 512
+
+# include "../libft/includes/libft.h"
+# include <readline/readline.h>
+# include <readline/history.h>
+
+
+# endif
