edc31e49137e974f0d9f0e0981fa5d359a200f0c
Daniel Haddington <operas.referee.0e@icloud.com>
2023-08-17 10:47:10 +0930

    structure

--- source/cmd.c
+++ source/cmd.c
@@ -6,7 +6,7 @@
 /*   By: dhadding <operas.referee.0e@icloud.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 10:38:08 by dhadding          #+#    #+#             */
-/*   Updated: 2023/08/17 10:41:37 by dhadding         ###   ########.fr       */
+/*   Updated: 2023/08/17 10:46:54 by dhadding         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,19 @@
 void run(int flag, t_cmd *cmd)
 {
 	if (flag == SIMPLECMD)
-	
+		run_simplecmd(cmd);
 	if (flag == COMPLEXCMD)
-
+		run_complex_cmd(cmd);
 	free(cmd->cmds);
 	free(cmd->pipredir);
+}
+
+void run_simplecmd(t_cmd *cmd)
+{
+
+}
+
+void run_complexcmd(t_cmd *cmd)
+{
+	
 }
