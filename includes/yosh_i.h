82adf38ed0b8699897923fb1c9ef3197dda8fa33
Daniel Haddington <operas.referee.0e@icloud.com>
2023-08-22 11:22:59 +0930

    update header

--- includes/yosh_i.h
+++ includes/yosh_i.h
@@ -6,7 +6,7 @@
 /*   By: dhadding <operas.referee.0e@icloud.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 10:31:51 by dhadding          #+#    #+#             */
-/*   Updated: 2023/08/18 09:52:43 by dhadding         ###   ########.fr       */
+/*   Updated: 2023/08/22 10:14:31 by dhadding         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,17 @@
 # include <readline/history.h>
 # include <libgen.h>
 # include <string.h>
+# include <fcntl.h>
 
 typedef struct s_cmd
 {
 	char	***cmds;
 	char	**tokens;
 	char	*pipredir;
+	int		end[2];
 }	t_cmd;
+
+extern char	**environ;
 
 /// BUILTIN1_C ///
 int		check_4_builtin(char **tokens);
@@ -42,9 +46,8 @@ int		exports(char **tokens);
 int		unset(char **tokens);
 int		env(void);
 
-/// CMD_C ///
-void	run(int flag, t_cmd *cmd);
-void	run_simplecmd(t_cmd *cmd);
+/// ENV_C ///
+char	*get_prog_path(char *prog_name);
 
 /// FREE_C ///
 void	free_2d(char **array);
@@ -53,6 +56,19 @@ void	free_3d(char ***array);
 /// PARSE_C ///
 int		parse_input(char *input, t_cmd *cmd);
 char	*find_piperedir(char **tokens);
+char	***cmd_list(char **tokens, char *pipredir);
+
+/// RUN1_C ///
+void	run(int flag, t_cmd *cmd);
+void	run_complexcmd(t_cmd *cmd, int a, int trigger);
+void	run_simplecmd(t_cmd *cmd);
+
+/// RUN2_C ///
+void	run_pipe(t_cmd *cmd, int a, int trigger);
+// void	run_redir_from_file(t_cmd *cmd, int a, int trigger);
+// void	run_redir_to_file(t_cmd *cmd, int a, int trigger);
+// void	run_redir_to_file_append(t_cmd *cmd, int a);
+// void	run_redir_from_file_find(t_cmd *cmd, int a);
 
 /// SETUP_C ///
 void	init_yosh_i(void);
