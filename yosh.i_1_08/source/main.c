541625016046c3f8001271778dd6adfe37e9d1f4
Daniel Haddington <operas.referee.0e@icloud.com>
2023-11-30 20:21:51 +1030

    sligth fix with main

--- yosh.i_1_08/source/main.c
+++ yosh.i_1_08/source/main.c
@@ -6,7 +6,7 @@
 /*   By: dhadding <operas.referee.0e@icloud.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 10:29:32 by dhadding          #+#    #+#             */
-/*   Updated: 2023/11/30 14:50:13 by dhadding         ###   ########.fr       */
+/*   Updated: 2023/11/30 20:12:17 by dhadding         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,27 +23,24 @@ void	init_struct(t_cmd **cmd, t_prompt **prompt, t_norm **norm)
 	(*cmd)->estatus = 0;
 }
 
-int	main(int argc, char **argv)
+int	main(void)
 {
 	t_cmd		*cmd;
 	t_prompt	*prompt;
 	t_norm		*norm;
 
-	if (argc == 1)
+	copy_environ(environ);
+	init_struct(&cmd, &prompt, &norm);
+	load_history();
+	entry_message();
+	while (1)
 	{
-		copy_environ(environ);
-		init_struct(&cmd, &prompt, &norm);
-		load_history();
-		entry_message();
-		while (1)
-		{
-			if (!cmd_input(cmd, prompt))
-				continue ;
-			cmd->expanded = (char *)malloc(sizeof(char) * INLEN);
-			cmd->expanded = expander(cmd->input, norm);
-			cmd->flag = parse_input(cmd, norm);
-			if (cmd->flag > 0)
-				run(cmd);
-		}
+		if (!cmd_input(cmd, prompt))
+			continue ;
+		cmd->expanded = (char *)malloc(sizeof(char) * INLEN);
+		cmd->expanded = expander(cmd->input, norm);
+		cmd->flag = parse_input(cmd, norm);
+		if (cmd->flag > 0)
+			run(cmd);
 	}
 }
